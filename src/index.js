134efaf1123ac0ab71f49047860c8d843013e6b7
Jehu Oki <jehuoki@gmail.com>
2023-02-13 19:01:19 +0100

    added deploy scripts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Tictactoe} from './tictactoe.js'
 
+console.log('hi')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
